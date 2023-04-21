d2deac6742ed6382584000f2be2e2154f0925299
Robbe Sneyders <robbe.sneyders@ml6.eu>
2023-04-21 10:16:31 +0200

    Format Terraform code

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_lambda"
+  name = "iam_lambda"
   assume_role_policy = jsonencode(
     {
       "Version" : "2012-10-17",
@@ -45,7 +45,7 @@ resource "aws_s3_bucket_notification" "incoming" {
 }
 
 resource "aws_lambda_permission" "s3_permission_to_trigger_lambda" {
-  count  = var.trigger_bucket_name != "null" ? 1 : 0
+  count         = var.trigger_bucket_name != "null" ? 1 : 0
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda_function.arn
@@ -55,7 +55,7 @@ resource "aws_lambda_permission" "s3_permission_to_trigger_lambda" {
 
 # Infrastructure setup for SQS trigger
 resource "aws_iam_role_policy_attachment" "lambda_function" {
-  count  = var.trigger_queue_arn != "null" ? 1 : 0
+  count      = var.trigger_queue_arn != "null" ? 1 : 0
   policy_arn = aws_iam_policy.lambda_function[0].arn
   role       = aws_iam_role.iam_for_lambda.name
 }
@@ -66,7 +66,7 @@ resource "aws_iam_policy" "lambda_function" {
 }
 
 data "aws_iam_policy_document" "lambda_function" {
-  count  = var.trigger_queue_arn != "null" ? 1 : 0
+  count = var.trigger_queue_arn != "null" ? 1 : 0
   statement {
     sid       = "AllowSQSPermissions"
     effect    = "Allow"
@@ -106,7 +106,7 @@ data "aws_iam_policy_document" "lambda_function" {
 }
 
 resource "aws_lambda_event_source_mapping" "event_source_mapping" {
-  count  = var.trigger_queue_arn != "null" ? 1 : 0
+  count            = var.trigger_queue_arn != "null" ? 1 : 0
   event_source_arn = var.trigger_queue_arn
   enabled          = true
   function_name    = aws_lambda_function.lambda_function.arn
@@ -116,20 +116,20 @@ resource "aws_lambda_event_source_mapping" "event_source_mapping" {
 
 # Infrastructure setup for HTTP trigger
 resource "aws_api_gateway_rest_api" "lambda_gateway" {
-  count  = var.http_trigger ? 1 : 0
+  count       = var.http_trigger ? 1 : 0
   name        = "ServerlessExample"
   description = "API Gateway to access serverless lambda function"
 }
 
 resource "aws_api_gateway_resource" "proxy" {
-  count  = var.http_trigger ? 1 : 0
+  count       = var.http_trigger ? 1 : 0
   rest_api_id = aws_api_gateway_rest_api.lambda_gateway[0].id
   parent_id   = aws_api_gateway_rest_api.lambda_gateway[0].root_resource_id
   path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "proxy" {
-  count  = var.http_trigger ? 1 : 0
+  count         = var.http_trigger ? 1 : 0
   rest_api_id   = aws_api_gateway_rest_api.lambda_gateway[0].id
   resource_id   = aws_api_gateway_resource.proxy[0].id
   http_method   = "ANY"
@@ -137,7 +137,7 @@ resource "aws_api_gateway_method" "proxy" {
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
-  count  = var.http_trigger ? 1 : 0
+  count         = var.http_trigger ? 1 : 0
   rest_api_id   = aws_api_gateway_rest_api.lambda_gateway[0].id
   resource_id   = aws_api_gateway_rest_api.lambda_gateway[0].root_resource_id
   http_method   = "ANY"
@@ -145,7 +145,7 @@ resource "aws_api_gateway_method" "proxy_root" {
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
-  count  = var.http_trigger ? 1 : 0
+  count       = var.http_trigger ? 1 : 0
   rest_api_id = aws_api_gateway_rest_api.lambda_gateway[0].id
   resource_id = aws_api_gateway_method.proxy_root[0].resource_id
   http_method = aws_api_gateway_method.proxy_root[0].http_method
@@ -156,7 +156,7 @@ resource "aws_api_gateway_integration" "lambda_root" {
 }
 
 resource "aws_api_gateway_integration" "lambda" {
-  count  = var.http_trigger ? 1 : 0
+  count       = var.http_trigger ? 1 : 0
   rest_api_id = aws_api_gateway_rest_api.lambda_gateway[0].id
   resource_id = aws_api_gateway_method.proxy[0].resource_id
   http_method = aws_api_gateway_method.proxy[0].http_method
@@ -167,7 +167,7 @@ resource "aws_api_gateway_integration" "lambda" {
 }
 
 resource "aws_api_gateway_deployment" "lambda_function" {
-  count  = var.http_trigger ? 1 : 0
+  count = var.http_trigger ? 1 : 0
   depends_on = [
     aws_api_gateway_integration.lambda,
     aws_api_gateway_integration.lambda_root,
@@ -179,7 +179,7 @@ resource "aws_api_gateway_deployment" "lambda_function" {
 
 
 resource "aws_lambda_permission" "apigw" {
-  count  = var.http_trigger ? 1 : 0
+  count         = var.http_trigger ? 1 : 0
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda_function.function_name
