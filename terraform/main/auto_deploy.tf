5ece7dbd755798a975170790e78f77178721ed0b
MiroGoettler <miro.goettler@miro.goettler@gmail.com>
2023-06-06 13:56:38 +0200

    Fix terraform format

--- terraform/main/auto_deploy.tf
+++ terraform/main/auto_deploy.tf
@@ -9,7 +9,7 @@ data "aws_iam_policy_document" "assume_role" {
     actions = ["sts:AssumeRole"]
 
     principals {
-      type = "Service"
+      type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
   }
